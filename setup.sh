aad0f1ed2dbf1e3d9e801d3b9574afeccd9a5f73
LazyWorm <641541452@qq.com>
2019-07-26 10:39:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ sudo ufw disable
 swapoff -a
 
 #hosts
-sudo echo "192.168.8.170 master-1
-192.168.9.173 worker-1
-192.168.9.174 worker-2
-192.168.9.175 worker-3" >> /etc/hosts
+sudo echo  "192.168.8.170 master1
+192.168.8.171 worker1
+192.168.8.172 worker2
+192.168.8.173 worker3" >> /etc/hosts
+
 
 sudo echo  "deb https://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main" >> /etc/apt/sources.list
 sudo apt-get update && apt-get install -y docker.io apt-transport-https curl kubelet kubeadm kubectl --allow-unauthenticated
